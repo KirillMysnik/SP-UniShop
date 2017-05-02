86b03486db7af441c571ffb4ba7aca8c33520083
KirillMysnik <kirill@mysnik.com>
2017-05-02 13:37:36 +0300

    Removed ItemClassMeta

--- srcds/addons/source-python/packages/custom/unishop/items.py
+++ srcds/addons/source-python/packages/custom/unishop/items.py
@@ -9,44 +9,19 @@ from .plugins import UniShopPlugin
 # =============================================================================
 # >> CLASSES
 # =============================================================================
-class ItemClassMeta:
-    def __init__(cls, name, bases, namespace):
-        super().__init__(name, bases, namespace)
-
-        if namespace.get('abstract', False):
-            del cls.abstract
-            return
-
-        if not hasattr(cls, 'unishop_plugin'):
-            raise ValueError("Class '{}' doesn't have 'unishop_plugin' "
-                             "attribute".format(cls))
-
-        if not hasattr(cls, 'id'):
-            raise ValueError(
-                "Class '{}' doesn't have 'id' attribute".format(cls))
-
-        if cls.unishop_plugin is None:
-            raise ValueError("Class '{}' has its 'unishop_plugin' "
-                             "attribute set to None".format(cls))
-
-        if cls.id is None:
-            raise ValueError("Class '{}' has its 'id' "
-                             "attribute set to None".format(cls))
-
-        if not isinstance(cls.unishop_plugin, UniShopPlugin):
-            raise ValueError("'unishop_pluign' attribute must be a "
-                             "valid UniShopPlugin instance")
-
-        cls.unishop_plugin.register_item_class(cls.id, cls)
-
-
-class ItemClass(metaclass=ItemClassMeta):
+class ItemClass:
     items_consumed_per_use = 1
 
     def __init__(self, unishop_plugin, id_, lang_strings):
+        if not isinstance(unishop_plugin, UniShopPlugin):
+            raise ValueError("'unishop_pluign' argument must be a "
+                             "valid UniShopPlugin instance")
+
         self.unishop_plugin = unishop_plugin
         self.id = id_
         self.lang_strings = lang_strings
+
+        unishop_plugin.register_item_class(id_, self)
 
     def get_item_icon(self, item):
         return '/'.join((
